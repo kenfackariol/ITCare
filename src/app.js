d6727f859130153fdc633e22448bf3d55782c69e
bayanga antoine <badinfo1987@gmail.com>
2024-07-17 15:08:55 +0100

    enhanced error handling

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ require('dotenv').config();
 
 const logger = require('./config/logger');
 const routes = require('./routes');
+const AppError = require('./utils/appError'); 
 
 const app = express();
 
@@ -28,5 +29,31 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(`/api/${process.env.API_VERSION}`, routes);
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  let errorResponse = {
+    status: err.status,
+    message: err.message,
+    code: err.statusCode,
+    // stack: err.stack // Always include the stack trace
+  };
+
+  if (err instanceof AppError) {
+    // If it's an operational error (AppError), include any additional properties
+    errorResponse = { ...errorResponse, ...err };
+  } else {
+    // For unexpected errors, you might want to log them server-side
+    console.error('Unexpected error:', err);
+    
+    // Optionally, you can set a generic message for unexpected errors
+    errorResponse.message = 'An unexpected error occurred';
+  }
+
+  res.status(err.statusCode).json(errorResponse);
+});
 
 module.exports = app;
